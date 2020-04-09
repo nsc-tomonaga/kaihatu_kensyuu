700818d22bf5b8196569dabe83bf46adecd9195e
nsc-tomonaga <63280570+nsc-tomonaga@users.noreply.github.com>
2020-04-09 17:23:13 +0900

    area.jsの追加
    課題１の追加

---
+++ js/area.js
@@ -0,0 +1,13 @@
+function getArea(length, width) {
+    let area = length * width;
+
+    return area;
+}
+
+function getPerimeter(length, width) {
+    let perimeter = length*2 + width*2; 
+    
+    return perimeter
+}
+console.log(getArea(3,4));
+console.log(getPerimeter(3,4));
