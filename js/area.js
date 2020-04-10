556f576ec9a808bf32f6ac4fcee56dc02200254b
nsc-tomonaga <63280570+nsc-tomonaga@users.noreply.github.com>
2020-04-10 16:19:45 +0900

    課題修正
    問題1の修正

--- js/area.js
+++ js/area.js
@@ -1,13 +1,22 @@
 function getArea(length, width) {
-    let area = length * width;
+    if (length >= 1 && length <= 1000){
+        if(width >= 1 && width <= 1000){
+            let area = length * width;
 
-    return area;
+            return area;
+        }
+    }
 }
 
 function getPerimeter(length, width) {
-    let perimeter = length*2 + width*2; 
+    if(length >= 1 && length <= 1000){
+        if(width >= 1 && width <= 1000){
+     
+            let perimeter = length*2 + width*2; 
     
-    return perimeter
+            return perimeter
+        }
+    }
 }
 console.log(getArea(3,4));
 console.log(getPerimeter(3,4));
